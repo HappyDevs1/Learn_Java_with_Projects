79dd50b712444b48b502038affdc2cc45f5bdda3
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-27 17:45:41 +0200

    Uploading chapter 1 code

---
+++ src/main/java/ch1/exercises/Exercise1_2.java
@@ -0,0 +1,9 @@
+package ch1.exercises;
+
+public class Exercise1_2 {
+    public static void main (String[] args) {
+        String name = "Happy";
+        String position = "Park manager";
+        System.out.println("My name is " + name + " and I want to be a " + position + " in Mesozoic Eden.");
+    }
+}
---
+++ src/main/java/ch1/projects/Entrance.java
@@ -0,0 +1,11 @@
+package ch1.projects;
+
+public class Entrance {
+  public static void main (String[] args) {
+    String welcomeMessage = "Welcome to Mesozoic Eden";
+    String openingHours = "08:00";
+    String closingHours = "20:00";
+
+    System.out.println(welcomeMessage + " the park opens from " + openingHours + " to " + closingHours + " Please ensure that you are wearing shoes that covers your feet for your own safety");
+  }
+}
