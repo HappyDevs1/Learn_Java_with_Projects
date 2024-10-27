368804989a76203a2da394acae2fcef6c98e92e7
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-27 18:13:11 +0200

    Uploading exercise 1.6

---
+++ src/main/java/ch1/exercises/Exercise1_6.java
@@ -0,0 +1,8 @@
+package ch1.exercises;
+
+public class Exercise1_6 {
+  public static void main (String[] args) {
+    String myName = "Happy";
+    System.out.println("Welcome, " + myName + " to Mesozoic Eden!" + " Mesozoic Eden is safe and secure.");
+  }
+}
