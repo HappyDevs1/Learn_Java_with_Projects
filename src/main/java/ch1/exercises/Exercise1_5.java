13edab5986cb501c310b37c0d2a83464cc261d42
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-27 18:02:34 +0200

    Uploading chapter 1.4 code

---
+++ src/main/java/ch1/exercises/Exercise1_5.java
@@ -0,0 +1,9 @@
+package ch1.exercises;
+
+public class Exercise1_5 {
+  public static void main (String[] args) {
+    String name = "Happy";
+
+    System.out.println("Welcome, " + name + " to Mesozoic Eden!");
+  }
+}
