2fc585a65cd3cf02dcc8cb58bbd22be433337fd6
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-29 13:10:11 +0200

    changes to the exercise

---
+++ src/main/java/ch2/projects/DinosaurProfileGenerator.java
@@ -0,0 +1,13 @@
+package ch2.projects;
+
+public class DinosaurProfileGenerator {
+  public static void main (String[] args) {
+    String name = "Katlin";
+    int age = 22;
+    String specie = "T-Rex";
+    String diet = "herbivore";
+    double weight = 400.32;
+
+    System.out.println("Meet " + name + ", a " + age + " year-old " + specie + ". As a " + diet + ", it has a robust weight of " + weight + " kilograms.");
+  }
+}
