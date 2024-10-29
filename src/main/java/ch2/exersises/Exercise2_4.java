b61823df8c84b15d45609888baa735af404297b6
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-29 11:59:27 +0200

    changes to the exercise

---
+++ src/main/java/ch2/exersises/Exercise2_4.java
@@ -0,0 +1,11 @@
+package ch2.exersises;
+
+public class Exercise2_4 {
+  public static void main (String[] args) {
+    String name = "Adam";
+    int age = 40;
+
+    System.out.println("Employee name: " + name);
+    System.out.println("Employee age: " + age);
+  }
+}
