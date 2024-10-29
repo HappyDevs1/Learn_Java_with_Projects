fe6657fe445dd6b2d73072aaa2e5fe4a83e80b07
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-30 00:29:00 +0200

    completed new exercise

---
+++ src/main/java/ch3/Exercise3_1.java
@@ -0,0 +1,12 @@
+package ch3;
+
+public class Exercise3_1 {
+  public static void main (String[] args) {
+    double dino1 = 367.50;
+    double dino2 = 380.67;
+
+    double average = dino1 + dino2;
+
+    System.out.println("Average weight of dinosaurs: " + average / 2);
+  }
+}
