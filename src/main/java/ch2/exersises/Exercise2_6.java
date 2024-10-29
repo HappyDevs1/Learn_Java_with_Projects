19415d2e715fd4330472f48a3de291e3355aea87
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-29 12:27:45 +0200

    changes to the exercise

---
+++ src/main/java/ch2/exersises/Exercise2_6.java
@@ -0,0 +1,9 @@
+package ch2.exersises;
+
+public class Exercise2_6 {
+  public static void main (String[] args) {
+    double rating = 9.80;
+
+    System.out.println("Safety rating: " + rating);
+  }
+}
