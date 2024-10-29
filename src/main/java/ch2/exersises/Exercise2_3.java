16d480e43626bcb22084ff1939b3271a58b4ab31
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-29 11:54:46 +0200

    updating exercise 2.1

---
+++ src/main/java/ch2/exersises/Exercise2_3.java
@@ -0,0 +1,9 @@
+package ch2.exersises;
+
+public class Exercise2_3 {
+  public static void main (String[] args) {
+    int maxPeople = 500;
+
+    System.out.println("There is a maximum number of " + maxPeople + " people allowed in Mesozoic Eden.");
+  }
+}
