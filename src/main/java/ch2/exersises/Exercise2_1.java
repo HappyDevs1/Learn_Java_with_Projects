33dab1793e9342f5e28185c91d5c170673ed4682
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-29 11:33:12 +0200

    exercise 2.1

---
+++ src/main/java/ch2/exersises/Exercise2_1.java
@@ -0,0 +1,12 @@
+package ch2.exersises;
+
+public class Exercise2_1 {
+  public static void main (String[]  args) {
+    String breed = "Carnivore";
+    int height = 23;
+    int length = 13;
+    int weight = 50;
+
+    System.out.println("Dinosaurs details: " + breed+ " , height: " + height + " , length: " + length + " , weight: " + weight);
+  }
+}
