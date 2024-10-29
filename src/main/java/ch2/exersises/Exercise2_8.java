00fafdf792cee728ef08470378c5ea8cbc38fc73
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-29 12:53:45 +0200

    changes to the exercise

---
+++ src/main/java/ch2/exersises/Exercise2_8.java
@@ -0,0 +1,9 @@
+package ch2.exersises;
+
+public class Exercise2_8 {
+  public static void main (String[] args) {
+    char specieFirstLetter = 'V';
+
+    System.out.println("First letter of Velociraptor: " + specieFirstLetter);
+  }
+}
