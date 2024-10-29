9306ab207b2415de892d2e4ebc853be8ae286ab5
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-29 12:45:42 +0200

    changes to the exercise

---
+++ src/main/java/ch2/exersises/Exercise2_7.java
@@ -0,0 +1,11 @@
+package ch2.exersises;
+
+public class Exercise2_7 {
+  public static void main (String[] args) {
+    String name = "Velociraptor";
+    int age = 20;
+    boolean isCarnivore = true;
+
+    System.out.println("Dinosaur name is " + name + " the age is " + age + " years old " + "the diet is " + (isCarnivore ? "carnivore" : "herbivore") );
+  }
+}
