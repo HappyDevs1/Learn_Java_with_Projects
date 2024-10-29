44e20d0bb01832356741dd75e853af7d9a262b2c
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-29 11:49:34 +0200

    updating exercise 2.1

---
+++ src/main/java/ch2/exersises/Exercise2_2.java
@@ -0,0 +1,13 @@
+package ch2.exersises;
+
+public class Exercise2_2 {
+  public static void main (String[] args) {
+    int age = 20;
+    String name = "Velociraptor";
+    boolean isCarnivore = true;
+
+    System.out.println("Age: " + age);
+    System.out.println("Name: " +  name);
+    System.out.println("Is carnivore: " + (isCarnivore ? "Yes" : "No"));
+  }
+}
