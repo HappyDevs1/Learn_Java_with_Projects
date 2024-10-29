eebf06dec570417b798f68da4ab25fee4f248600
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-29 12:24:31 +0200

    changes to the exercise

---
+++ src/main/java/ch2/exersises/Exercise2_5.java
@@ -0,0 +1,9 @@
+package ch2.exersises;
+
+public class Exercise2_5 {
+  public static void main (String[] args) {
+    int totalDinosaurs = 22;
+
+    System.out.println("Total Dinosaurs: " + totalDinosaurs);
+  }
+}
