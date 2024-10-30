17ab58e98bde54599c64270874198424e47a8b1a
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-30 22:37:08 +0200

    adding new chapter 4 exercise

---
+++ src/main/java/ch4/exercises/Exercise4_1.java
@@ -0,0 +1,13 @@
+package ch4.exercises;
+
+public class Exercise4_1 {
+  public static void main (String[] args) {
+    boolean isCarnivore = true;
+
+    if (isCarnivore) {
+      System.out.println("The dinosaur is a carnivore");
+    } else {
+      System.out.println("The dinosaur is a herbivore");
+    }
+  }
+}
