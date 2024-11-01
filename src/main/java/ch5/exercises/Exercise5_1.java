486d6b3e8c212d40d65d7673ae6fcaabd2ba462c
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-11-02 00:43:53 +0200

    adding new chapter 5 exercise

--- src/main/java/ch5/exercises/Exercise5_1.java
+++ src/main/java/ch5/exercises/Exercise5_1.java
@@ -4,7 +4,7 @@ public class Exercise5_1 {
   public static void main (String[] args) {
     int i;
     for (i = 1; i <= 100; i++) {
-      System.out.println(i);
+      System.out.println("Dino"+i);
     }
   }
 }
