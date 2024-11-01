7b3db518a4bb8320dc328e58ea10bf3da302baf9
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-11-02 00:42:13 +0200

    adding new chapter 4 exercise

---
+++ src/main/java/ch5/exercises/Exercise5_1.java
@@ -0,0 +1,10 @@
+package ch5.exercises;
+
+public class Exercise5_1 {
+  public static void main (String[] args) {
+    int i;
+    for (i = 1; i <= 100; i++) {
+      System.out.println(i);
+    }
+  }
+}
