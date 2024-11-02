fd575f2505af8346cd6f9e5aec1e4f798ffd6d0b
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-11-02 08:19:57 +0200

    adding new chapter 5 exercise

---
+++ src/main/java/ch5/exercises/Exercise5_6.java
@@ -0,0 +1,11 @@
+package ch5.exercises;
+
+public class Exercise5_6 {
+  public static void main (String[] args) {
+    boolean allPass = false;
+    do {
+      System.out.println("Security check");
+      allPass = true;
+    } while (!allPass);
+  }
+}
