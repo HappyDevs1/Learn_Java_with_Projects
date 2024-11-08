f6aac4f7cc748c616f83fe9fa838512bb2b6920f
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-11-08 22:22:46 +0200

    New completed exercise

---
+++ src/main/java/ch6/exercises/Exercise6_1.java
@@ -0,0 +1,10 @@
+package ch6.exercises;
+
+public class Exercise6_1 {
+  public static void main (String[] args) {
+    String[] dinoNames = {"Velociraptor", "Brontosaurus", "Spinosaurus", "Diplodocus" };
+    for (String dinoName: dinoNames) {
+      System.out.println("Available dinosaur: " + dinoName);
+    }
+  }
+}
