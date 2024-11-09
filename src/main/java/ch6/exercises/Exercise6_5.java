9086f72353016db99fb770dc6183208d72099582
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-11-09 20:51:25 +0200

    New completed exercise

---
+++ src/main/java/ch6/exercises/Exercise6_5.java
@@ -0,0 +1,11 @@
+package ch6.exercises;
+
+public class Exercise6_5 {
+  public static void main(String[] args) {
+    String[] employeesNames = {"David", "Marx", "Rebecca", "Katlin", "Junior"};
+
+    for(String name: employeesNames) {
+      System.out.println(name);
+    }
+  }
+}
