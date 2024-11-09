23237361a44b032453a63b9b40a187e810ac3298
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-11-09 21:23:29 +0200

    New completed exercise

---
+++ src/main/java/ch6/exercises/Exercise6_6.java
@@ -0,0 +1,10 @@
+package ch6.exercises;
+
+public class Exercise6_6 {
+  public static void main(String[] args) {
+    String[][] map = {
+            {"Entrance", "Playground", "Covered picnic area", "Museum"},
+            {"Dinosaur walk begin", "Boneyard", "Dinosaur photo head", "Fossil dig"}
+    };
+  }
+}
